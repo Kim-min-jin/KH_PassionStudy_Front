2447e4c68b95654a971f486c34072d461fae7782
Ethan_kim <hey0507@gmail.com>
2020-12-15 23:09:26 +0900

    edit folders JG

--- Frontend_done/JS/menubar.js
+++ Frontend_done/JS/menubar.js
@@ -1,31 +1,39 @@
 /****헤더 마우스따라 움직이는 바****/
 function headerBar() {
-  var onMenu = $("header #menu li.on").offset().left;
+  var onMenu = $(
+    ".PassionStudy_header #PassionStudy_header_menu li.on"
+  ).offset().left;
 
   $(".floting_bar").offset({ left: onMenu });
 
-  $("header #menu li").on("mouseenter", function () {
-    var a = $(this).offset().left;
-    $(".floting_bar").offset({ left: a });
-  });
-  $("header #menu li").on("mouseleave", function () {
-    $(".floting_bar").offset({ left: onMenu });
-  });
+  $(".PassionStudy_header #PassionStudy_header_menu li").on(
+    "mouseenter",
+    function () {
+      var a = $(this).offset().left;
+      $(".floting_bar").offset({ left: a });
+    }
+  );
+  $(".PassionStudy_header #PassionStudy_header_menu li").on(
+    "mouseleave",
+    function () {
+      $(".floting_bar").offset({ left: onMenu });
+    }
+  );
 }
 
 function checkWindowWidth() {
   var windowWidth = $(window).width();
   if (windowWidth < 1000) {
     //창 가로 크기가 1000 미만일 경우
-    $("header #menu li.pc").removeClass("on");
+    $(".PassionStudy_header #PassionStudy_header_menu li.pc").removeClass("on");
     if ($("header #menu li.job.mo").length == 0) {
       $("#menu-main .job.mo").addClass("on");
     }
   } else {
     //창 가로 크기가 1000보다 클 경우
-    $("header #menu li.mo").removeClass("on");
-    if ($("header #menu li.pc").length == 0) {
-      $("header #menu li.pc").addClass("on");
+    $(".PassionStudy_header #PassionStudy_header_menu li.mo").removeClass("on");
+    if ($(".PassionStudy_header #PassionStudy_header_menu li.pc").length == 0) {
+      $(".PassionStudy_header #PassionStudy_header_menu li.pc").addClass("on");
     }
   }
 }
@@ -36,15 +44,17 @@ $(function () {
   headerBar();
 
   /****헤더클릭 페이지 바로가기+중앙배치****/
-  $("header #menu li a.pagemove").click(function () {
-    var makeCenter = ($(window).height() - $(this.hash).outerHeight()) / 2;
-    $("html,body").animate(
-      {
-        scrollTop: $(this.hash).offset().top - makeCenter,
-      },
-      400
-    );
-  });
+  $(".PassionStudy_header #PassionStudy_header_menu li a.pagemove").click(
+    function () {
+      var makeCenter = ($(window).height() - $(this.hash).outerHeight()) / 2;
+      $("html,body").animate(
+        {
+          scrollTop: $(this.hash).offset().top - makeCenter,
+        },
+        400
+      );
+    }
+  );
 });
 
 /****창크기 변화 감지 후 작동****/
